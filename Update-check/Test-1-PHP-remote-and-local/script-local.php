fadb3cc286af5f51463dfeef8c75d9752b3b43f6
Basile <6933795+php-dev-9300@users.noreply.github.com>
2024-08-02 02:22:16 +0200

    Update script-local.php

--- Update-check/Test-1-PHP-remote-and-local/script-local.php
+++ Update-check/Test-1-PHP-remote-and-local/script-local.php
@@ -14,14 +14,17 @@ echo "OK is same";
 <?php
 // 2 Files
 // File $a in local and file $b in remote server
-$a = file_get_contents('2.php');
-$b = file_get_contents('https://raw.githubusercontent.com/php-dev-9300/Snippets/main/Update-check/Test-1-PHP-remote-and-local/remote-example.php');
+$a = file_get_contents('https://raw.githubusercontent.com/php-dev-9300/Snippets/main/Update-check/Test-1-PHP-remote-and-local/remote-example.php');
+$b = file_get_contents('https://www.php-dev.ovh/test/check-last-modified/update-locale-from-remote/remote-call.php');
+$a = rtrim($a);
+$b = rtrim($b);
 $a_hash = md5($a);
 $b_hash = md5($b);
-if($a_hash !== $b_hash) 
+if($a_hash !== $b_hash) {
 echo "NOK is different";
-else 
+}else{ 
 echo "OK is same";
+}
 ?>
 
 <?php 
